851d751581e434c630d6791a5868868ad7a5930f
Ahmad Ali <xevenahmad@gmail.com>
2024-10-18 13:18:27 +0500

    adding main routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 // App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        {/* Add other routes here */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
